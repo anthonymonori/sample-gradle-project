3d20830c081e71f48310dd4341c11d73ddb4b23f
Antal Monori <antal.monori@transferwise.com>
2024-12-30 09:58:09 +0000

    Migrate to convention based Gradle Plugins in a build-logic module

---
+++ build-logic/src/main/kotlin/convention/SampleRootPlugin.kt
@@ -0,0 +1,14 @@
+package convention
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+internal class SampleRootPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        require(project == project.rootProject) {
+            "Plugin must be applied to root project!"
+        }
+
+        project.plugins.apply("com.jraska.module.graph.assertion")
+    }
+}
